68e0d253abba5d1410a8274fb3a551b0f8b03140
Mokenyu <mokenyujunior@gmail.com>
2026-04-12 02:48:48 -0400

    Add CORS to Function App; gitignore python build artifacts

--- IaC-files/function_app.tf
+++ IaC-files/function_app.tf
@@ -54,6 +54,11 @@ resource "azurerm_linux_function_app" "func" {
     application_stack {
       python_version = "3.11"
     }
+
+    cors {
+      allowed_origins     = ["https://portal.azure.com"]
+      support_credentials = false
+    }
   }
 
   app_settings = {
